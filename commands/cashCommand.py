026d25bfb03633f7320fe2d55b1aafd61881c7c4
AllocatedYT <49845020+AllocatedYT@users.noreply.github.com>
2020-09-19 21:00:18 -0700

    Update cashCommand.py

--- commands/cashCommand.py
+++ commands/cashCommand.py
@@ -1,1 +1,13 @@
+from commands.base_command import BaseCommand
+import random
 
+class Cash(BaseCommand):
+    def __init__(self):
+        description = "Gives you Qwertycoin."
+        params = ["multiplier"]
+        super().__init__(description, params)
+
+    async def handle(self, params, message, client):
+      multi = params[0]
+      gain = random.randint(1,301)*multi
+      
