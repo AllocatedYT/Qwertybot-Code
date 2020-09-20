ca59a8ca155630da53eb8a0f2ef3d3c58668c1de
AllocatedYT <49845020+AllocatedYT@users.noreply.github.com>
2020-09-19 20:22:26 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ import os
 
 # The prefix that will be used to parse commands.
 # It doesn't have to be a single character!
-COMMAND_PREFIX = "!"
+COMMAND_PREFIX = "q!"
 
 # The bot token. Keep this secret!
-BOT_TOKEN = "Don't let others know your token!"
+BOT_TOKEN = "NzU2OTc3NDIzNzU2ODIwNTcy.X2ZsKQ.HgZ943kJhe4Otjz64kJ3i5biY3w"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "commands"
